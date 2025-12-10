67b36b4a3f226142b2fb0260ef68cee769a78493
Adam Beguelin <adam@code.pr>
2025-12-10 12:55:45 -0400

    sending new users emails.

--- addons/cpr_membership/controllers/stripe_webhook.py
+++ addons/cpr_membership/controllers/stripe_webhook.py
@@ -1,6 +1,6 @@
 import json
 import logging
-from odoo import http
+from odoo import http, fields
 from odoo.http import request
 
 _logger = logging.getLogger(__name__)
@@ -19,6 +19,7 @@ class StripeWebhookController(http.Controller):
             customer_email = session.get('customer_details', {}).get('email')
             customer_name = session.get('customer_details', {}).get('name')
             amount = session.get('amount_total', 0) / 100
+            product_name = session.get('metadata', {}).get('product_name', 'Day Pass')
             
             _logger.info("Checkout completed: %s - %s - $%s", customer_name, customer_email, amount)
             
@@ -32,7 +33,81 @@ class StripeWebhookController(http.Controller):
                         'email': customer_email,
                     })
                     _logger.info("Created new contact: %s", partner.name)
+                    
+                    # Create portal user and send invitation
+                    self._create_portal_user(partner)
                 else:
                     _logger.info("Found existing contact: %s", partner.name)
+                
+                # Create invoice
+                if amount > 0:
+                    self._create_invoice(partner, amount, product_name, session.get('id'))
         
         return {'status': 'ok'}
+    
+    def _create_portal_user(self, partner):
+        """Create a portal user and send invitation email"""
+        try:
+            User = request.env['res.users'].sudo()
+            existing_user = User.search([('login', '=', partner.email)], limit=1)
+            
+            if existing_user:
+                _logger.info("User already exists for %s", partner.email)
+                return
+            
+            user = User.create({
+                'login': partner.email,
+                'partner_id': partner.id,
+                'groups_id': [(6, 0, [request.env.ref('base.group_portal').id])],
+            })
+            
+            user.action_reset_password()
+            
+            _logger.info("Created portal user and sent invitation to %s", partner.email)
+            
+        except Exception as e:
+            _logger.error("Failed to create portal user: %s", str(e))
+    
+    def _create_invoice(self, partner, amount, product_name, stripe_session_id):
+        """Create and post an invoice for the Stripe payment"""
+        try:
+            Invoice = request.env['account.move'].sudo()
+            
+            invoice = Invoice.create({
+                'move_type': 'out_invoice',
+                'partner_id': partner.id,
+                'invoice_date': fields.Date.today(),
+                'ref': f'Stripe: {stripe_session_id}',
+                'invoice_line_ids': [(0, 0, {
+                    'name': product_name,
+                    'quantity': 1,
+                    'price_unit': amount,
+                })],
+            })
+            
+            invoice.action_post()
+            self._register_payment(invoice, amount)
+            
+            _logger.info("Created invoice %s for %s", invoice.name, partner.name)
+            
+        except Exception as e:
+            _logger.error("Failed to create invoice: %s", str(e))
+    
+    def _register_payment(self, invoice, amount):
+        """Mark the invoice as paid"""
+        try:
+            Payment = request.env['account.payment.register'].sudo()
+            
+            payment_wizard = Payment.with_context(
+                active_model='account.move',
+                active_ids=invoice.ids
+            ).create({
+                'amount': amount,
+                'payment_date': fields.Date.today(),
+            })
+            payment_wizard.action_create_payments()
+            
+            _logger.info("Payment registered for invoice %s", invoice.name)
+            
+        except Exception as e:
+            _logger.error("Failed to register payment: %s", str(e))
